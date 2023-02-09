1ae59475fc870bb99f1682c6d3c429c88131c73b
Alan (Chun-Wei) Chen <86547379+890144chunwei@users.noreply.github.com>
2023-02-09 17:18:57 +0800

    Update PartI-master.sh

--- PartI-master.sh
+++ PartI-master.sh
@@ -8,17 +8,30 @@
 
 pwd; hostname; date
 
+BCLDIR="/storage/goodell/bcl/220601_Chunwei_RNA_Trx/Files"
 FASTQDIR="/storage/goodell/projects/chunweic/220519_ATACseq_LSK_1"
 OUTDIR="/storage/goodell/projects/chunweic/220519_ATACseq_LSK_1/Align"
 HOMEDIR="/storage/goodell/home/chunweic"
 
-trimmomatic PE -threads 1 -phred33 \
-$FASTQDIR/Trx_WT1_R1.fastq $FASTQDIR/Trx_WT1_R2.fastq \
-$OUTDIR/Trx_WT1_R1_paired.fastq $OUTDIR/Trx_WT1_R1_unpaired.fastq $OUTDIR/Trx_WT1_R2_paired.fastq $OUTDIR/Trx_WT1_R2_unpaired.fastq \
-ILLUMINACLIP:$HOMEDIR/adapter_trim/NexteraPE-PE.fa:2:15:4:4:true LEADING:20 TRAILING:20 SLIDINGWINDOW:4:15 MINLEN:25
+bcl2fastq -R $BCLDIR -i "$BCLDIR/Data/Intensities/BaseCalls/" -o $FASTQDIR --sample-sheet "$PROJECTDIR/SampleSheet_220601.csv" --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --ignore-missing-controls --loading-threads 8 -p 16 --tiles s_[1]
 
-bowtie2 --dovetail --phred33 -x $HOMEDIR/mm10/BowtieIndex/mm10/mm10 -1 $OUTDIR/Trx_WT1_R1_paired.fastq -2 $OUTDIR/Trx_WT1_R2_paired.fastq -S $OUTDIR/Trx_WT1.sam
+cp -r "$FASTQDIR/220601" $PROJECTDIR
+fastqc $PROJECTDIR/*gz
+gunzip $PROJECTDIR/*gz
+multiqc $PROJECTDIR/*zip -o $PROJECTDIR/
 
-samtools view -S -b $OUTDIR/Trx_WT1.sam > $OUTDIR/Trx_WT1.bam
-samtools sort $OUTDIR/Trx_WT1.bam $OUTDIR/Trx_WT1_sort
-samtools index $OUTDIR/Trx_WT1_sort.bam
+for FILE in $PROJECTDIR/*R1.fastq ;
+do
+  trimmomatic PE -threads 1 -phred33 \
+  $FASTQDIR/Trx_WT1_R1.fastq $FASTQDIR/Trx_WT1_R2.fastq \
+  $OUTDIR/Trx_WT1_R1_paired.fastq $OUTDIR/Trx_WT1_R1_unpaired.fastq $OUTDIR/Trx_WT1_R2_paired.fastq $OUTDIR/Trx_WT1_R2_unpaired.fastq \
+  ILLUMINACLIP:$HOMEDIR/adapter_trim/NexteraPE-PE.fa:2:15:4:4:true LEADING:20 TRAILING:20 SLIDINGWINDOW:4:15 MINLEN:25
+
+  bowtie2 --dovetail --phred33 -x $HOMEDIR/mm10/BowtieIndex/mm10/mm10 -1 $OUTDIR/Trx_WT1_R1_paired.fastq -2 $OUTDIR/Trx_WT1_R2_paired.fastq -S $OUTDIR/Trx_WT1.sam
+
+  samtools view -S -b $OUTDIR/Trx_WT1.sam > $OUTDIR/Trx_WT1.bam
+  samtools sort $OUTDIR/Trx_WT1.bam $OUTDIR/Trx_WT1_sort
+  samtools index $OUTDIR/Trx_WT1_sort.bam
+done
+
+rm $PROJECTDIR/*fastq $$PROJECTDIR/*zip $$PROJECTDIR/*out.bamn
